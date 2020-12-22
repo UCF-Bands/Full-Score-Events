ea112b7fed9b279c4d36deabe5c3d8d518410b2d
Jordan Pakrosnis <jpak@jordanpak.com>
2020-12-21 23:39:00 -0500

    Clean up schedule items comments/classes

--- src/templates/block/schedule-items.php
+++ src/templates/block/schedule-items.php
@@ -17,11 +17,11 @@ $items = $dom->getElementsByTagName( 'li' );
 $day   = false;
 ?>
 
-<div class="fse-schedule">
+<div class="fse-schedule-items">
 	<?php
 	foreach ( $items as $item ) {
 
-		// Grab the timestamp and output a new date.
+		// Grab the timestamp and see if we need to output a new date.
 		$time = $item->getElementsByTagName( 'time' );
 
 		// Loop is to avoid error.
@@ -34,9 +34,10 @@ $day   = false;
 
 		if ( $day !== $item_day ) {
 			echo '<h4 class="fse-schedule-day">' . esc_html( $item_day ) . '</h4>';
-			echo '<ul class="fse-schedule-items">';
+			echo '<ul class="fse-schedule-day-items">';
 		}
 
+		// Output item contents.
 		echo $dom->saveHTML( $item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 		if ( $day !== $item_day ) {
