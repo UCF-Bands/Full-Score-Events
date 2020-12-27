30d47e50ff0e04b84c6c15b9a8101d37f5d0a2a7
Jordan Pakrosnis <jpak@jordanpak.com>
2020-12-27 11:29:23 -0500

    Sanity check content and dateTime in schedule item before rendering

--- src/templates/block/schedule-item.php
+++ src/templates/block/schedule-item.php
@@ -6,11 +6,17 @@
  * @package Full_Score_Events
  */
 
+$content = trim( $content );
+
+if ( ! $content || ! $dateTime ) {
+	return;
+}
+
 $time = new DateTime( $dateTime );
 $time = gmdate( 'g:i a', $time->getTimestamp() );
 ?>
 
 <li class="fse-schedule-item">
 	<time datetime="<?php echo esc_attr( $dateTime ); ?>"><?php echo esc_html( $time ); ?></time>
-	<div class="fse-schedule-activity"><?php echo trim( $content ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></div>
+	<div class="fse-schedule-activity"><?php echo $content; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></div>
 </li>
