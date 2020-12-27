ba7e62da9a9cc0d78c62b8cd815198f5fb39a4b9
Jordan Pakrosnis <jpak@jordanpak.com>
2020-12-27 11:29:53 -0500

    Init rendered schedule block

--- src/templates/block/schedule.php
+++ src/templates/block/schedule.php
@@ -6,5 +6,29 @@
  * @package Full_Score_Events
  */
 
+global $fse_schedule;
+
+$edit_block = $fse_schedule->get_edit_block();
+
+if ( ! $edit_block ) {
+	return;
+}
+
 ?>
-<h2>Look at this schedule</h2>
+
+<div class="fse-schedule">
+	<?php
+	foreach ( $edit_block['innerBlocks'] as $block ) :
+		if ( 'full-score-events/schedule-items' === $block['blockName'] && $block['innerBlocks'] ) :
+			?>
+			<ul class="fse-schedule-items">
+				<?php echo implode( '', array_map( 'render_block', $block['innerBlocks'] ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+			</ul>
+			<?php
+		else :
+			echo render_block( $block ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		endif;
+
+	endforeach;
+	?>
+</div>
