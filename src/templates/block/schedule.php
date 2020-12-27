60c4876f3955917b34d52ef67a5a8f4c791b48f2
Jordan Pakrosnis <jpak@jordanpak.com>
2020-12-27 11:56:01 -0500

    Add download button to schedule output

--- src/templates/block/schedule.php
+++ src/templates/block/schedule.php
@@ -6,6 +6,8 @@
  * @package Full_Score_Events
  */
 
+namespace Full_Score_Events;
+
 global $fse_schedule;
 
 $edit_block = $fse_schedule->get_edit_block();
@@ -14,6 +16,7 @@ if ( ! $edit_block ) {
 	return;
 }
 
+$upload_id = $edit_block['attrs']['uploadId'] ?? false;
 ?>
 
 <div class="fse-schedule">
@@ -31,4 +34,26 @@ if ( ! $edit_block ) {
 
 	endforeach;
 	?>
+
+	<?php
+	if ( $upload_id ) :
+		$a_attrs = apply_filters(
+			'full_score_events_schedule_download_attrs',
+			[
+				'href'     => wp_get_attachment_url( $upload_id ),
+				'class'    => 'button',
+				'download' => 'download',
+			]
+		);
+		?>
+		<a <?php do_attrs( $a_attrs ); ?>>
+			<?php
+			echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+				'full_score_events_schedule_download_text',
+				__( 'Download', 'full-score-events' ),
+				$upload_id
+			);
+			?>
+		</a>
+	<?php endif; ?>
 </div>
