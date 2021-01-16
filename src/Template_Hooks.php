328a86158a5255f4b7ea08b891d575fa66bc0a23
Jordan Pakrosnis <jpak@jordanpak.com>
2021-01-16 14:25:41 -0500

    Create template hook handler class

---
+++ src/Template_Hooks.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template hooking
+ *
+ * @since   1.0.0
+ * @package Full_Score_Events
+ */
+
+namespace Full_Score_Events;
+
+// exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Template hooks
+ *
+ * Action/filter hooks used for WooCommerce functions/templates.
+ *
+ * @since 1.0.0
+ */
+class Template_Hooks {
+
+	/**
+	 * Hook things in
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		add_action( 'full_score_events_before_main_content', 'Full_Score_Events\output_content_wrapper' );
+		add_action( 'full_score_events_after_main_content', 'Full_Score_Events\output_content_wrapper_close' );
+	}
+}
