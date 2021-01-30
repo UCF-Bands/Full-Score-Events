b73c96c407167e712242bd55507e5572555ef80e
Jordan Pakrosnis <jpak@jordanpak.com>
2021-01-30 15:18:27 -0500

    Init single event content template with title

---
+++ src/templates/content-single-event.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template for displaying the events archive
+ *
+ * This template can be overridden by copying it to
+ * yourtheme/full-score-events/content-single-event.php
+ *
+ * However, Full Score Events may need to update template files and you (the
+ * theme developer) will need to copy the new file to your theme to maintain
+ * compatibility. It is recommended that you make your customizations using
+ * hooks/filters to reduce technical debt.
+ *
+ * @package Full_Score_Events/templates
+ * @since   1.0.0
+ */
+
+the_title( '<h2>', '</h2>' );
