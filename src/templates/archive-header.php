c1e0c2da3d8f77737a5b39bac9f97c336436900e
Jordan Pakrosnis <jpak@jordanpak.com>
2021-01-30 15:23:09 -0500

    Create archive header template

---
+++ src/templates/archive-header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template for displaying the events archive header
+ *
+ * This template can be overridden by copying it to
+ * yourtheme/full-score-events/archive-header.php
+ *
+ * However, Full Score Events may need to update template files and you (the
+ * theme developer) will need to copy the new file to your theme to maintain
+ * compatibility. It is recommended that you make your customizations using
+ * hooks/filters to reduce technical debt.
+ *
+ * @package Full_Score_Events/templates
+ * @since   1.0.0
+ */
+
+use function Full_Score_Events\do_attrs_class;
+
+global $post_type;
+
+$post_type_obj = get_post_type_object( $post_type );
+?>
+
+<header <?php do_attrs_class( 'fse-archive-header', "{$post_type}-archive-header" ); ?>>
+
+	<h1 <?php do_attrs_class( 'fse-archive-header-title', "{$post_type}-archive-header-title", 'page-title' ); ?>><?php echo esc_html( $post_type_obj->label ); ?></h1>
+
+</header>
