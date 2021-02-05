13a1e08b6644a2b67355edaa78bcd8582f5e3273
Jordan Pakrosnis <jpak@jordanpak.com>
2021-02-04 23:12:00 -0500

    Add events loop event content template

---
+++ src/templates/content-event.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template for displaying the contents of an event in an archive loop
+ *
+ * This template can be overridden by copying it to
+ * yourtheme/full-score-events/content-event.php
+ *
+ * However, Full Score Events may need to update template files and you (the
+ * theme developer) will need to copy the new file to your theme to maintain
+ * compatibility. It is recommended that you make your customizations using
+ * hooks/filters to reduce technical debt.
+ *
+ * @package Full_Score_Events/templates
+ * @since   1.0.0
+ */
+
+namespace Full_Score_Events;
+
+/**
+ * Hook: full_score_events_loop_before_event
+ */
+do_action( 'full_score_events_loop_before_event' );
+?>
+
+<article id="fse-event-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php
+	/**
+	 * Hook: full_score_events_loop_event
+	 *
+	 * @hooked Full_Score_Events\do_loop_event_header - 10
+	 * @hooked Full_Score_Events\do_event_excerpt - 20
+	 *
+	 * @since 1.0.0
+	 */
+	do_action( 'full_score_events_loop_event_content' );
+	?>
+</article>
+
+<?php
+/**
+ * Hook: full_score_events_loop_after_event
+ */
+do_action( 'full_score_events_loop_after_event' );
