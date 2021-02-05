6994dcc27dbd3dfb27219121fae1af0e6cc675fd
Jordan Pakrosnis <jpak@jordanpak.com>
2021-02-04 23:10:34 -0500

    Add excerpt support to event

--- src/Events.php
+++ src/Events.php
@@ -124,7 +124,7 @@ class Events extends Post_Type {
 				'items_list_navigation' => __( 'Events list navigation', 'full-score-events' ),
 				'filter_items_list'     => __( 'Filter events list', 'full-score-events' ),
 			],
-			'supports'            => [ 'title', 'editor', 'thumbnail', 'custom-fields', 'revisions' ],
+			'supports'            => [ 'title', 'editor', 'excerpt', 'thumbnail', 'custom-fields', 'revisions' ],
 			'public'              => true,
 			'show_ui'             => true,
 			'menu_icon'           => 'dashicons-calendar-alt',
