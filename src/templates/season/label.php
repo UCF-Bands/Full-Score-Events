f3273d6ebdf69685a8bc8991a37032c73697c584
Jordan Pakrosnis <jpak@jordanpak.com>
2021-02-13 14:33:10 -0500

    Add season label template

---
+++ src/templates/season/label.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template for displaying a season's "label".
+ *
+ * This template can be overridden by copying it to
+ * yourtheme/full-score-events/season/label.php
+ *
+ * However, Full Score Events may need to update template files and you (the
+ * theme developer) will need to copy the new file to your theme to maintain
+ * compatibility. It is recommended that you make your customizations using
+ * hooks/filters to reduce technical debt.
+ *
+ * @param integer $id    Season term ID.
+ * @param string  $date  Season term "start date" meta (Ymd).
+ * @param string  $name  Season term name.
+ *
+ * @package Full_Score_Events/templates
+ * @since   1.0.0
+ */
+
+$date = DateTime::createFromFormat( 'Ymd', $date );
+?>
+
+<h3 class="fse-season-label">
+	<?php echo esc_html( $name ); ?>
+	<small class="fse-season-label-year">
+		<?php echo esc_html( $date->format( 'Y' ) ); ?>
+	</small>
+</h3>
