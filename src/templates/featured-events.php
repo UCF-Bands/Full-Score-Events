3eb89d7f1fe8c5bb9135d618d6234f8d10051836
Jordan Pakrosnis <jpak@jordanpak.com>
2021-02-17 21:08:52 -0500

    Add template parts for featured events and its header

---
+++ src/templates/featured-events.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template for displaying the loop of featured events
+ *
+ * This template can be overridden by copying it to
+ * yourtheme/full-score-events/featured-events.php
+ *
+ * However, Full Score Events may need to update template files and you (the
+ * theme developer) will need to copy the new file to your theme to maintain
+ * compatibility. It is recommended that you make your customizations using
+ * hooks/filters to reduce technical debt.
+ *
+ * @package Full_Score_Events/templates
+ * @since   1.0.0
+ */
+
+namespace Full_Score_Events;
+
+$events = Events::get_featured();
+
+if ( ! $events->have_posts() ) {
+	return;
+}
+?>
+
+<section class="fse-featured-events">
+
+	<header class="fse-featured-events-header">
+		<?php
+		/**
+		 * Hook: full_score_events_featured_header_content
+		 *
+		 * @hooked Full_Score_Events\do_featured_heading - 10
+		 * @hooked Full_Score_Events\do_featured_body - 20
+		 */
+		do_action( 'full_score_events_featured_header_content' );
+		?>
+	</header>
+
+	<ul class="fse-featured-events-list">
+		<?php
+		while ( $events->have_posts() ) :
+			$events->the_post();
+			?>
+			<li class="fse-featured-events-list-event">
+				<?php get_plugin_template( 'content', 'event' ); ?>
+			</li>
+		<?php endwhile; ?>
+	</ul>
+
+</section>
+
+<?php
+wp_reset_postdata();
