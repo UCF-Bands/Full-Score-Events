6b287e00e164359b63c5530b20ed886b455eb734
Jordan Pakrosnis <jpak@jordanpak.com>
2021-02-21 12:32:14 -0500

    Add background and wrap to featured-events template

--- src/templates/featured-events.php
+++ src/templates/featured-events.php
@@ -27,31 +27,41 @@ $background = Customizer::get( 'featured_background' );
 
 <section class="fse-featured-events">
 
-	<?php echo wp_get_attachment_image( $background, [ 1600, 1000 ], false, [ 'class' => 'fse-featured-events-background' ] ); ?>
+	<?php
+	echo wp_get_attachment_image(
+		$background,
+		[ 1600, 1000 ],
+		false,
+		[ 'class' => 'fse-featured-events-background fse-cover-image' ]
+	);
+	?>
 
-	<header class="fse-featured-events-header">
-		<?php
-		/**
-		 * Hook: full_score_events_featured_header_content
-		 *
-		 * @hooked Full_Score_Events\do_featured_heading - 10
-		 * @hooked Full_Score_Events\do_featured_body - 20
-		 */
-		do_action( 'full_score_events_featured_header_content' );
-		?>
-	</header>
+	<div class="fse-wrap fse-featured-events-wrap">
 
-	<ul class="fse-featured-events-list">
-		<?php
-		while ( $events->have_posts() ) :
-			$events->the_post();
+		<header <?php do_attrs_class( 'fse-featured-events-header', $background ? 'fse-has-background' : null ); ?>>
+			<?php
+			/**
+			 * Hook: full_score_events_featured_header_content
+			 *
+			 * @hooked Full_Score_Events\do_featured_heading - 10
+			 * @hooked Full_Score_Events\do_featured_body - 20
+			 */
+			do_action( 'full_score_events_featured_header_content' );
 			?>
-			<li class="fse-featured-events-list-event">
-				<?php get_plugin_template( 'content', 'event' ); ?>
-			</li>
-		<?php endwhile; ?>
-	</ul>
+		</header>
 
+		<ul class="fse-featured-events-list">
+			<?php
+			while ( $events->have_posts() ) :
+				$events->the_post();
+				?>
+				<li class="fse-featured-events-list-event">
+					<?php get_plugin_template( 'content', 'event' ); ?>
+				</li>
+			<?php endwhile; ?>
+		</ul>
+
+	</div>
 </section>
 
 <?php
