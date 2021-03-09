c7a7fb1f2f729768347e58bfe755601370218a17
Jordan Pakrosnis <jpak@jordanpak.com>
2021-03-08 23:25:13 -0500

    Clean up event time template

--- src/templates/event/time.php
+++ src/templates/event/time.php
@@ -25,26 +25,20 @@ if ( ! $fse_event->get_time_start() ) {
 ?>
 
 <span class="fse-event-time">
+	<?php do_icon( 'clock' ); ?>
+
 	<?php
-	do_icon( 'clock' );
-	echo ' ';
-
 	if ( $fse_event->is_time_tba() ) :
-		echo esc_html__( 'TBA', 'full-score-events' ) . '</span>';
-		return;
-	endif;
-
-	if ( $fse_event->is_daily() ) :
+		esc_html_e( 'TBA', 'full-score-events' );
+	elseif ( $fse_event->is_daily() ) :
 		esc_html_e( 'Daily', 'full-score-events' );
 	else :
 		?>
 		<time datetime="<?php $fse_event->do_time_start( 'attr' ); ?>"><?php $fse_event->do_time_start(); ?></time>
-		<?php
-	endif;
 
-	if ( $fse_event->get_show_finish() && $fse_event->get_day_start() === $fse_event->get_day_finish() ) :
-		?>
-		<span class="fse-time-separator"> – </span>
-		<time datetime="<?php $fse_event->do_time_finish( 'attr' ); ?>"><?php $fse_event->do_time_finish(); ?></time>
+		<?php if ( $fse_event->get_show_finish() && $fse_event->get_day_start() === $fse_event->get_day_finish() ) : ?>
+			<span class="fse-time-separator"> – </span>
+			<time datetime="<?php $fse_event->do_time_finish( 'attr' ); ?>"><?php $fse_event->do_time_finish(); ?></time>
+		<?php endif; ?>
 	<?php endif; ?>
 </span>
