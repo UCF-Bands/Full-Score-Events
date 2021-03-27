b460185db31e1033b195f8c708fa0bb60f06ca79
Jordan Pakrosnis <jpak@jordanpak.com>
2021-03-27 16:05:36 -0400

    move ensemble withSelect handling to its own utility

--- src/editor/blocks/upcoming-events/edit.js
+++ src/editor/blocks/upcoming-events/edit.js
@@ -6,79 +6,73 @@
 
 import ServerSideRender from '@wordpress/server-side-render';
 import { __ } from '@wordpress/i18n';
-import { withSelect } from '@wordpress/data';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, TextControl, TextareaControl } from '@wordpress/components';
 
+import withEnsembles from '../../util/with-ensembles';
 import EnsemblesControl from '../../components/ensembles-control';
 
-const edit = withSelect( ( select, { attributes } ) => ( {
-	ensembles: select( 'core' ).getEntityRecords( 'taxonomy', 'fse_ensemble', {
-		include: attributes.ensembles,
-	} ),
-	ensembleSuggestions: select( 'core' ).getEntityRecords(
-		'taxonomy',
-		'fse_ensemble'
-	),
-} ) )( ( { attributes, setAttributes, ensembles, ensembleSuggestions } ) => {
-	const { number, noneFound } = attributes;
+const edit = withEnsembles(
+	( { attributes, setAttributes, ensembles, ensembleSuggestions } ) => {
+		const { number, noneFound } = attributes;
 
-	// set default arrays in case there aren't any available yet
-	ensembleSuggestions = ensembleSuggestions || [];
+		// set default arrays in case there aren't any available yet
+		ensembleSuggestions = ensembleSuggestions || [];
 
-	const numberControl = (
-		<TextControl
-			label={ __( 'Number to show', 'full-score-events' ) }
-			type="number"
-			min={ 1 }
-			step={ 1 }
-			value={ number }
-			onChange={ ( value ) =>
-				setAttributes( { number: Number( value ) } )
-			}
-		/>
-	);
+		const numberControl = (
+			<TextControl
+				label={ __( 'Number to show', 'full-score-events' ) }
+				type="number"
+				min={ 1 }
+				step={ 1 }
+				value={ number }
+				onChange={ ( value ) =>
+					setAttributes( { number: Number( value ) } )
+				}
+			/>
+		);
 
-	const noneFoundControl = (
-		<TextareaControl
-			label={ __( '"None found" message', 'full-score-events' ) }
-			value={ noneFound }
-			placeholder={ __(
-				"Ex: There aren't any scheduled events at this time.",
-				'full-score-events'
-			) }
-			onChange={ ( value ) => setAttributes( { noneFound: value } ) }
-			help={ `${ __(
-				"Leave empty to not display block if there aren't any upcoming events found.",
-				'full-score-events'
-			) } ${ fullScoreEventsEditor.allowedInlineHTML }` }
-		/>
-	);
+		const noneFoundControl = (
+			<TextareaControl
+				label={ __( '"None found" message', 'full-score-events' ) }
+				value={ noneFound }
+				placeholder={ __(
+					"Ex: There aren't any scheduled events at this time.",
+					'full-score-events'
+				) }
+				onChange={ ( value ) => setAttributes( { noneFound: value } ) }
+				help={ `${ __(
+					"Leave empty to not display block if there aren't any upcoming events found.",
+					'full-score-events'
+				) } ${ fullScoreEventsEditor.allowedInlineHTML }` }
+			/>
+		);
 
-	return (
-		<>
-			<InspectorControls>
-				<PanelBody title={ __( 'Layout', 'full-score-events' ) }>
-					{ numberControl }
-					{ noneFoundControl }
-					<EnsemblesControl
-						ensembles={ ensembles }
-						suggestions={ ensembleSuggestions }
-						setEnsembles={ ( options ) => {
-							setAttributes( { ensembles: options } );
-						} }
+		return (
+			<>
+				<InspectorControls>
+					<PanelBody title={ __( 'Layout', 'full-score-events' ) }>
+						{ numberControl }
+						{ noneFoundControl }
+						<EnsemblesControl
+							ensembles={ ensembles }
+							suggestions={ ensembleSuggestions }
+							setEnsembles={ ( options ) => {
+								setAttributes( { ensembles: options } );
+							} }
+						/>
+					</PanelBody>
+				</InspectorControls>
+
+				<div { ...useBlockProps() }>
+					<ServerSideRender
+						block="full-score-events/upcoming-events"
+						attributes={ attributes }
 					/>
-				</PanelBody>
-			</InspectorControls>
-
-			<div { ...useBlockProps() }>
-				<ServerSideRender
-					block="full-score-events/upcoming-events"
-					attributes={ attributes }
-				/>
-			</div>
-		</>
-	);
-} );
+				</div>
+			</>
+		);
+	}
+);
 
 export default edit;
