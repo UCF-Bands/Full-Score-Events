9c05f2cae0eff20f33e6374223d2ce18a0a1d1b4
Jordan Pakrosnis <jpak@jordanpak.com>
2026-06-26 21:59:55 -0400

    Return blank Location map embed if there's no map src (place)

--- src/Location.php
+++ src/Location.php
@@ -113,13 +113,19 @@ class Location extends Post {
 	 */
 	public function get_map_embed() {
 
+		$map_src = $this->get_map_src();
+
+		if ( ! $map_src ) {
+			return '';
+		}
+
 		$attrs = [
 			'class'       => 'fse-location-map',
 			'title'       => __( 'Location map', 'full-score-events' ),
 			'width'       => '100%',
 			'height'      => '405', // @todo make this a block setting.
 			'frameborder' => '0',
-			'src'         => $this->get_map_src(),
+			'src'         => $map_src,
 		];
 
 		return '<iframe ' . get_attrs( $attrs ) . '></iframe>';
