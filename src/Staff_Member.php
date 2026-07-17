567d7fba6e0ba60110721f08e680433372554bc0
Jordan Pakrosnis <jpak@jordanpak.com>
2026-07-17 18:42:20 -0400

    Init social getters/outputters in Staff_Member

--- src/Staff_Member.php
+++ src/Staff_Member.php
@@ -101,4 +101,86 @@ class Staff_Member extends Post {
 	public function do_phone_display() {
 		echo esc_html( $this->get_phone_display() );
 	}
+
+	/**
+	 * Get user-facing Facebook link label/text
+	 *
+	 * @since 1.1.0
+	 *
+	 * @return string
+	 */
+	public function get_facebook_label() {
+		return $this->get( '_facebook_label' ) ?: __( 'Facebook', 'full-score-events' );
+	}
+
+	/**
+	 * Output user-facing Facebook link label/text
+	 *
+	 * @since 1.1.0
+	 */
+	public function do_facebook_label() {
+		echo esc_html( $this->get_facebook_label() );
+	}
+
+	/**
+	 * Get Facebook URL
+	 *
+	 * @since 1.1.0
+	 *
+	 * @return string
+	 */
+	public function get_facebook_url() {
+		return esc_url( $this->get( '_facebook_url' ) );
+	}
+
+	/**
+	 * Output Facebook URL
+	 *
+	 * @since 1.1.0
+	 */
+	public function do_facebook_url() {
+ 		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo $this->get_facebook_url();
+	}
+
+	/**
+	 * Get user-facing Discord link label/text
+	 *
+	 * @since 1.1.0
+	 *
+	 * @return string
+	 */
+	public function get_discord_label() {
+		return $this->get( '_discord_label' ) ?: __( 'Discord', 'full-score-events' );
+	}
+
+	/**
+	 * Output user-facing Facebook link label/text
+	 *
+	 * @since 1.1.0
+	 */
+	public function do_discord_label() {
+		echo esc_html( $this->get_discord_label() );
+	}
+
+	/**
+	 * Get Discord URL
+	 *
+	 * @since 1.1.0
+	 *
+	 * @return string
+	 */
+	public function get_discord_url() {
+		return esc_url( $this->get( '_discord_url' ) );
+	}
+
+	/**
+	 * Output Facebook URL
+	 *
+	 * @since 1.1.0
+	 */
+	public function do_discord_url() {
+ 		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo $this->get_discord_url();
+	}
 }
